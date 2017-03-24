02bd99b909e11c36e5a8da407eaff8e843413db1
mattcaswell <matthewcaswell253@Gmail.com>
2017-03-24 11:21:31 -0700

    add dynamic routing to pages

--- app/router.js
+++ app/router.js
@@ -7,6 +7,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('question');
+  this.route('question', {path: '/question/:question_id'});
+
 });
 
 export default Router;
